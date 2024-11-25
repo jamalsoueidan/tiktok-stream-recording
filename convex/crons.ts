7c81fe733e12f08172f192093b8d58af3588b332
Jamal Soueidan <jamal@soueidan.com>
2024-11-25 19:16:48 +0100

    remove cron for now

--- convex/crons.ts
+++ convex/crons.ts
@@ -1,8 +1,7 @@
 import { cronJobs } from "convex/server";
-import { internal } from "./_generated/api";
 
 const crons = cronJobs();
 
-crons.interval("check who is online", { minutes: 5 }, internal.tiktok.checkAll);
+//crons.interval("check who is online", { minutes: 5 }, internal.tiktok.checkAll);
 
 export default crons;
